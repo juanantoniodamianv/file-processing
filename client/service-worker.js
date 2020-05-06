6d9ca61fc64b32e2195ea6d758b328787be1dc4f
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2020-05-06 08:40:22 -0300

    Update the app title

--- client/service-worker.js
+++ client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ed9201117f41a8ccb5024c2acd02b06.js"
+  "/precache-manifest.8f35ccdc3ed745a1a6d5d7b50ed3e9ec.js"
 );
 
 self.addEventListener('message', (event) => {
