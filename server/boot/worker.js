d55894bf27e35058cbb0e59d30f58475d8c008db
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2020-07-02 09:52:27 -0300

    Update worker time

--- server/boot/worker.js
+++ server/boot/worker.js
@@ -68,7 +68,7 @@ module.exports = function(app) {
 
   fileProcessor.add({},{
     repeat: {
-      cron: '12 11 * * *'
+      cron: '30 3 * * *'
     }
   }); 
   
@@ -88,7 +88,7 @@ module.exports = function(app) {
 
   fileProcessorPdfFiles.add({ foo: 'bar' },{
     repeat: {
-      cron: '15 11 * * *'
+      cron: '35 3 * * *'
     }
   });
 }
